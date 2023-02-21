38db390bde5e2e9881bb5a9fe5c4ce2b67e33d43
miadafe <95299624+miadafe@users.noreply.github.com>
2023-02-21 16:45:56 +0000

    txt-to-arff-v1.2
    parses all 3 txts successfully to arffs

--- parse-DD.py
+++ parse-DD.py
@@ -9,6 +9,23 @@ with open("dewey.arff", "w", encoding="utf-8") as f:
     "@DATA\n"
   )
 
+#should i be doing one big arff or seperate? seperate for now...
+with open("universal.arff", "w", encoding="utf-8") as u:
+    u.write(
+    "@RELATION dewey\n\n"
+    "@ATTRIBUTE text   string\n"
+    "@ATTRIBUTE class  {CH,IS,EA,HD,BU,JU,OT,GE}\n\n"
+    "@DATA\n"
+  )
+
+with open("leeds.arff", "w", encoding="utf-8") as l:
+    l.write(
+    "@RELATION dewey\n\n"
+    "@ATTRIBUTE text   string\n"
+    "@ATTRIBUTE class  {CH,IS,EA,HD,BU,JU,OT,GE}\n\n"
+    "@DATA\n"
+  )
+
 religions = ["CH","IS","EA","HD","BU","JU","OT","GE"]
 system = ["DD", "LU", "UD"]
 
@@ -34,21 +51,70 @@ def deweyParse(para):
     return 0
 
 def leedsParse(para):
+    with open("leeds.arff", "a", encoding="utf-8") as l:
+        for sentence in para:
+            #is removing punc still necessary?:
+            sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+            #add quote mark to beginning of sentence
+            sentence = '" ' + sentence
+            code = sentence.split()
+            #code[1] represents the number part of the line
+            if ((code[1][:1]) == 'A'):
+                sentence += "\", GE\n"
+                l.write(sentence)
+            elif (code[1][:1] == 'J'):
+                sentence += "\", OT\n"
+                l.write(sentence)
+            else:
+                sentence += "\", CH\n"
+                l.write(sentence)
     return 0
 
 def universalParse(para):
+    with open("universal.arff", "a", encoding="utf-8") as u:
+        for sentence in para:
+            #is removing punc still necessary?:
+            sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+            #add quote mark to beginning of sentence
+            sentence = '" ' + sentence
+            code = sentence.split()
+            #code[1] represents the number part of the line
+            # print(code[1][:2])
+            if ((code[1][:2]) == '21' or int(code[1][:2]) == '25'):
+                sentence += "\", GE\n"
+                u.write(sentence)
+            elif (code[1][:2] == '22'):
+                sentence += "\", EA\n"
+                u.write(sentence)
+            elif (code[1][:2] == '23'):
+                sentence += "\", HD\n"
+                u.write(sentence)
+            elif (code[1][:2] == '24'):
+                sentence += "\", BU\n"
+                u.write(sentence)
+            elif (code[1][:2] == '26'):
+                sentence += "\", JU\n"
+                u.write(sentence)
+            elif (code[1][:2] == '27'):
+                sentence += "\", CH\n"
+                u.write(sentence)
+            elif (code[1][:2] == '28'):
+                sentence += "\", IS\n"
+                u.write(sentence)
+            else:
+                sentence += "\", OT\n"
+                u.write(sentence)
     return 0
 
 
 with open("RE-" + system[0] + ".txt", "r", encoding="utf-8") as f:
-    # para = f.readlines()
     para = f.read().splitlines()
     deweyParse(para)
 
 with open("RE-" + system[1] + ".txt", "r", encoding="utf-8") as f:
-    para = f.readlines()
+    para = f.read().splitlines()
     leedsParse(para)
 
 with open("RE-" + system[2] + ".txt", "r", encoding="utf-8") as f:
-    para = f.readlines()
+    para = f.read().splitlines()
     universalParse(para)
