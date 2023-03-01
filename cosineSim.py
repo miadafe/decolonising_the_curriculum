fc8d5bd3dc96ff64ee1bca2fd7cccb52f137f3ee
miadafe <95299624+miadafe@users.noreply.github.com>
2023-03-01 13:56:44 +0000

    cosine_simiari

--- cosineSim.py
+++ cosineSim.py
@@ -1,25 +1,38 @@
 import string
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
 from nltk.corpus import stopwords
 stopwords = stopwords.words('english')
 
+# so this actually compares each list element as a document.. so i need the whole document as one elem and a list with 3 elems for 3 documents
 
-# countvectorizer converts strings to numerical vectors
-# stopwords removes most common words (I, the etc)
+corpus = ['', '', '']
 
-# have sentences in list form: = ['first sentence', 'next one']
+def addDeweyToCorp():
+    with open("dewey.txt", "r", encoding="utf-8") as f:
+         deweypara = f.read().splitlines()
+    for i in range(len(deweypara)):
+        corpus[0] = corpus[0] + deweypara[i] + " "
 
-def rm_punc_stopwords(text):
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    text = text.lower
-    text = ' '.join([word for word in text.split() if word not in stopwords])
-    return text
+def addLeedsToCorp():
+    with open("leeds.txt", "r", encoding="utf-8") as f:
+         leedspara = f.read().splitlines()
+    for i in range(len(leedspara)):
+        corpus[1] = corpus[1] + leedspara[i] + " "
 
+def addUniToCorp():
+    with open("universal.txt", "r", encoding="utf-8") as f:
+         unipara = f.read().splitlines()
+    for i in range(len(unipara)):
+        corpus[2] = corpus[2] + unipara[i] + " "
 
-def vectorizer(clean_text):
-    vectorizer = CountVectorizer().fit_transform(clean_text)
-    vectors = vectorizer.toarray()
-    return vectors
+addDeweyToCorp()
+addLeedsToCorp()
+addUniToCorp()
 
-csim = cosine_similarity(vectors)
+vect = TfidfVectorizer(min_df=1, stop_words="english")
+tfidf = vect.fit_transform(corpus)
+pairwise_similarity = tfidf * tfidf.T
+print(pairwise_similarity)
