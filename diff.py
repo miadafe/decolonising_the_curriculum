c7d698aeb70ac179338fc5134b57d5a1e1af9553
miadafe <95299624+miadafe@users.noreply.github.com>
2023-04-16 14:12:15 +0100

    Add files via upload
    v1.1 - non-symmetric, normalisation and scaling

--- diff.py
+++ diff.py
@@ -26,9 +26,55 @@ addDeweyToCorp()
 addLeedsToCorp()
 addUniToCorp()
 
-comp1 = SequenceMatcher(a=corpus[0], b=corpus[1]) #0.007680196613033294
-comp2 = SequenceMatcher(a=corpus[1], b=corpus[2]) #0.003763538283547762
-comp3 = SequenceMatcher(a=corpus[0], b=corpus[2]) #0.009095948397500504
-print (comp1.ratio())
-print (comp2.ratio())
-print (comp3.ratio())
+comparisions = [0] *6
+ratios = [0] * 6
+comparisions[0] = SequenceMatcher(a=corpus[0], b=corpus[1]) #0.007680196613033294
+comparisions[1] = SequenceMatcher(a=corpus[0], b=corpus[2]) #0.01720364041319458
+
+
+comparisions[2] = SequenceMatcher(a=corpus[1], b=corpus[0]) #0.003763538283547762
+comparisions[3] = SequenceMatcher(a=corpus[1], b=corpus[2]) #0.004237465326661184
+
+
+comparisions[4] = SequenceMatcher(a=corpus[2], b=corpus[0]) #0.009095948397500504
+comparisions[5] = SequenceMatcher(a=corpus[2], b=corpus[1]) #0.005669219915339649
+
+def find_ratios():
+    for i in range (6):
+        ratios[i] = comparisions[i].ratio()
+
+find_ratios()
+
+# normalizing
+# normalized = [0] * 6
+# for i in range(6):
+#     # normalized[i] = (ratios[i] - min(ratios))/(max(ratios)-min(ratios))
+#     print(ratios[i])
+
+# scaling
+scaled = [0] * 6
+for i in range(6):
+    scaled[i] = ratios[i] * 10
+    print(scaled[i])
+
+def write_similarities():
+    with open("diff_similarities.txt", "w", encoding="utf-8") as f:
+        f.write("1")
+        f.write("\n")
+        f.write(str(scaled[0]))
+        f.write("\n")
+        f.write(str(scaled[1]))
+        f.write("\n")
+        f.write(str(scaled[2]))
+        f.write("\n")
+        f.write("1")
+        f.write("\n")
+        f.write(str(scaled[3]))
+        f.write("\n")
+        f.write(str(scaled[4]))
+        f.write("\n")
+        f.write(str(scaled[5]))
+        f.write("\n")
+        f.write("1")
+
+write_similarities()
